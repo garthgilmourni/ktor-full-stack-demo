8942ea8d6e077e1d48d04dc4444a0653c0777a12
Márton Braun <marton.braun@jetbrains.com>
2023-12-01 20:42:15 +0100

    Add some UI changes

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,69 +1,52 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import com.kmp.ktor.model.Task
 import com.kmp.ktor.network.TaskApi
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.TextButton
-import androidx.compose.ui.graphics.Color
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
         val client = remember { TaskApi() }
-        val tasks = remember { mutableStateOf(emptyList<Task>()) }
+        var tasks by remember { mutableStateOf(emptyList<Task>()) }
         val scope = rememberCoroutineScope()
 
         LaunchedEffect(Unit) {
-            tasks.value = client.getAllTasks()
+            tasks = client.getAllTasks()
         }
 
-        LazyColumn(modifier = Modifier.height(400.dp)) {
-            items(tasks.value) { task ->
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+            items(tasks) { task ->
                 Card(
-                    modifier = Modifier.padding(10.dp),
-                    shape = RoundedCornerShape(CornerSize(16.dp))
+                    modifier = Modifier.fillMaxWidth().padding(4.dp),
+                    shape = RoundedCornerShape(CornerSize(4.dp))
                 ) {
                     Column(modifier = Modifier.padding(10.dp)) {
-                        Text(task.name)
-                        Text(task.description)
-                        Text(task.priority.name)
+                        Text("${task.name}: ${task.description}")
+                        Text(task.priority.name, fontStyle = FontStyle.Italic)
 
                         Row {
-                            TextButton(onClick = {
+                            Button(onClick = {
                                 scope.launch {
                                     client.removeTask(task)
-                                    tasks.value = client.getAllTasks()
+                                    tasks = client.getAllTasks()
                                 }
                             }) {
                                 Text("Delete Task")
                             }
-                            TextButton(onClick = {
+                            Spacer(Modifier.width(8.dp))
+                            Button(onClick = {
                                 scope.launch {
                                     client.updateTask(task)
                                 }
